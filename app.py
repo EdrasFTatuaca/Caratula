3b5d19e7febfb1ef90d9c9350f1da2c7de0bb8d6
EdrasFTatuaca <etatuacaa@miumg.edu.gt>
2026-02-01 00:19:46 -0600

    primera plantilla

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Generador de carátulas funcionando"
+    datos = {
+        "titulo": "ANÁLISIS DE SISTEMAS",
+        "nombre": "Edras Fernando.",
+        "curso": "Ingeniería en Sistemas",
+        "docente": "Ing. Juan Pérez",
+        "institucion": "Universidad",
+        "fecha": "15 de enero de 2026"
+    }
+    return render_template("caratula.html", **datos)
 
 if __name__ == "__main__":
     app.run(debug=True)
